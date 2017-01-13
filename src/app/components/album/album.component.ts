f4ff9461652af41d7ed05eb894cc0ba0814e406b
Paul Julian <bpj101@dameagnesproductions.com>
2017-01-12 22:04:23 -0800

    Updates: Added album component to display albums and tracks

--- src/app/components/album/album.component.ts
+++ src/app/components/album/album.component.ts
@@ -1,3 +1,7 @@
+import { SpotifyService } from './../../services/spotify.service';
+import { ActivatedRoute } from '@angular/router';
+import { Album } from './../../../album';
+import { Artist } from './../../../artist';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./album.component.css']
 })
 export class AlbumComponent implements OnInit {
+  public id: string;
+  public album: Album[];
 
-  constructor() { }
+  constructor(
+    private _spotifyService: SpotifyService,
+    private _route: ActivatedRoute) { }
+  
 
   ngOnInit() {
+    this._route.params
+      .map(params => params['id'])
+      .subscribe((id) => {
+        console.log(id);
+        this._spotifyService.getAlbum(id)
+          .subscribe((album) => {
+              this.album = album;
+              console.log(this.album);
+          });
+      });
   }
 
 }
